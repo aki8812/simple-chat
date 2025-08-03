23b2e14c173ee0816024dc47457666dfc4b00438
aki8812 <akihuang0815@gmail.com>
2025-08-04 00:06:18 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,17 +3,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-storage.js";
 
-
 const firebaseConfig = {
   apiKey: "AIzaSyB9XkO17IHNggQwL__8ock3KCZOlBP8wcA",
   authDomain: "simple-chat-41e8d.firebaseapp.com",
   databaseURL: "https://simple-chat-41e8d-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "simple-chat-41e8d",
-  storageBucket: "simple-chat-41e8d.appspot.com", 
+  storageBucket: "simple-chat-41e8d.appspot.com",
   messagingSenderId: "181779135449",
   appId: "1:181779135449:web:6e87dcfd5a72b2c1b1f7c8"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
