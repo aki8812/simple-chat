6096ba893552cc9fdbecc00f92d8074a20108806
aki8812 <akihuang0815@gmail.com>
2025-10-11 18:41:24 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,8 +8,8 @@ const urlsToCache = [
   '/style2.css',
   '/firebase.js',
   '/favicon.ico',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/icon/icon-192x192.png',
+  '/icon/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -55,4 +55,5 @@ self.addEventListener('fetch', event => {
         );
       })
   );
+
 });
